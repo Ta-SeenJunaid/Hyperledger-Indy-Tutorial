84e3dc8c5efb77d6f9ffc5f94d54572ef1700265
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-09-14 21:22:14 +0600

    Static Method

---
+++ indy network/create_domain_ledger_genesis_file.py
@@ -0,0 +1,33 @@
+import argparse
+
+
+class DomainLedger:
+
+    @staticmethod
+    def _bootstrap_args_type_dids(dids_str_arg):
+        did = []
+        i =1 
+        for arg in dids_str_arg.split(','):
+            arg = str(arg)
+            arg = arg.strip()
+            if len(arg) != 22:
+                raise argparse.ArgumentTypeError("The lenght of did {} should be 22 digit long".format(i))           
+            did.append(arg)
+            i += 1
+
+        return did
+
+    
+    @staticmethod
+    def _bootstrap_args_type_verkeys(verkeys_str_arg):
+        verkeys = []
+        i =1 
+        for arg in verkeys_str_arg.split(','):
+            arg = str(arg)
+            arg = arg.strip()
+            if len(arg) != 23:
+                raise argparse.ArgumentTypeError("The lenght verification key {} should be 23 digit long".format(i))           
+            verkeys.append(arg)
+            i += 1
+
+        return verkeys
