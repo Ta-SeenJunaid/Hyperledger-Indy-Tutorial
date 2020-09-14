5333929f3cb31483ff141e9b0f99dd6a7e1e7be4
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-09-14 21:26:31 +0600

    Arguments parsing

--- indy network/create_domain_ledger_genesis_file.py
+++ indy network/create_domain_ledger_genesis_file.py
@@ -1,5 +1,11 @@
 import argparse
 
+from indy_common.config_helper import ConfigHelper
+from indy_common.config_util import getConfig
+from indy_common.txn_util import getTxnOrderedFields
+
+from plenum.common.config_helper import PConfigHelper
+
 
 class DomainLedger:
 
@@ -31,3 +37,32 @@ class DomainLedger:
             i += 1
 
         return verkeys
+
+
+    @classmethod
+    def bootstrap_domain_ledger(cls, config=getConfig(), config_helper_class=PConfigHelper,
+                                domainTxnFieldOrder=getTxnOrderedFields(), chroot: str=None):
+        parser = argparse.ArgumentParser(description="Generate domain transactions")
+        parser.add_argument('--trusteeDids', required=True,
+                            help='Trustee Dids, provide comma separated dids',
+                            type=cls._bootstrap_args_type_dids)
+        parser.add_argument('--trusteeVerkeys', required=True,
+                            help='Trustees Verkey, provide comma separated verification keys',
+                            type=cls. _bootstrap_args_type_verkeys)
+        parser.add_argument('--stewardDids', required=True,
+                            help='Stewards Dids, provide comma separated dids',
+                            type=cls._bootstrap_args_type_dids)
+        parser.add_argument('--stewardVerkeys', required=True,
+                            help='Stewards Verkey, provide comma separated verification keys',
+                            type=cls. _bootstrap_args_type_verkeys)
+        parser.add_argument('--network', required=False,
+                            help='Network name (default sandbox)',
+                            type=str,               
+                            default="sandbox")
+        parser.add_argument(
+            '--appendToLedgers',
+            help="Determine if ledger files needs to be erased "
+                 "before writing new information or not",
+            action='store_true')  
+
+        args = parser.parse_args()
