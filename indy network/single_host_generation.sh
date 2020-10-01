19c136be2003cf32865571084836ba608cd022dc
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-10-01 20:55:51 +0600

    create_domain_ledger_genesis_file

--- indy network/single_host_generation.sh
+++ indy network/single_host_generation.sh
@@ -6,3 +6,15 @@ sudo python3 init_indy_node.py \
 
 sudo python3 init_indy_node.py \
 --name Node2 --seed 300000A00u000z0000600003000Node3
+
+
+sudo python3 create_domain_ledger_genesis_file.py \
+--stewardDids \
+"RnXJMDxifkBaceAkPizz8F, AmJHyEZ9ofrPz1DUfx97Qo, PeVR5pdFizbK8WN9THd3mq" \
+--stewardVerkeys \
+"~9F4P9aQysR1YATnARMuhxt, ~CVv3mmR7QPanynNMJmTG9f, ~VPwSf359RqMB6TP5X728WJ" \
+--trusteeDids \
+"L4vaBJDbZiaX68mVHLuZ2E" \
+--trusteeVerkeys \
+"~QKV8UWR7LHB7APvrWc88xK" \
+--network sandbox
