ad104de222c3c1bbc8d08e4bdfe57454e2e7af5f
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-10-01 20:48:12 +0600

    Permission related to .sh file

---
+++ indy network/single_host_generation.sh
@@ -0,0 +1,8 @@
+sudo python3 init_indy_node.py \
+--name Node1 --seed 4000F000u00000D0000000g0000Node1
+
+sudo python3 init_indy_node.py \
+--name Node2 --seed T00000000u0000I0000v0003000Node2
+
+sudo python3 init_indy_node.py \
+--name Node2 --seed 300000A00u000z0000600003000Node3
