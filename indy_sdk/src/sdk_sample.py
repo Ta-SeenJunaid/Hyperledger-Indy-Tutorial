87ac897086d124e06eec69e70ee1881e5a8427a8
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-10-16 23:11:12 +0600

    sdk_sample.py

---
+++ indy_sdk/src/sdk_sample.py
@@ -0,0 +1,16 @@
+import logging
+
+import argparse
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+parser = argparse.ArgumentParser(description='Run python sdk_sample scenario')
+parser.add_argument('-t', '--storage_type', help='load custom wallet storage plug-in')
+parser.add_argument('-l', '--library', help='dynamic library to load for plug-in')
+parser.add_argument('-e', '--entrypoint', help='entry point for dynamic library')
+parser.add_argument('-c', '--config', help='entry point for dynamic library')
+parser.add_argument('-s', '--creds', help='entry point for dynamic library')
+
+args = parser.parse_args()
