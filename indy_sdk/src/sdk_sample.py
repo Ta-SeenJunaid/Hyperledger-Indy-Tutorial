592dd8373ecd11079459d30f101aa12e1811979d
Ta-SeenJunaid <taseen.junaid@gmail.com>
2020-10-17 08:56:19 +0600

    Initialization

--- indy_sdk/src/sdk_sample.py
+++ indy_sdk/src/sdk_sample.py
@@ -1,6 +1,12 @@
+import time
+
 import logging
 
 import argparse
+import sys
+from ctypes import *
+
+from src.utils import run_coroutine
 
 
 logger = logging.getLogger(__name__)
@@ -14,3 +20,35 @@ parser.add_argument('-c', '--config', help='entry point for dynamic library')
 parser.add_argument('-s', '--creds', help='entry point for dynamic library')
 
 args = parser.parse_args()
+
+if args.storage_type:
+    if not (args.library and args.entrypoint):
+        parser.print_help()
+        sys.exit(0)
+    stg_lib = CDLL(args.library)
+    result = stg_lib[args.entrypoint]()
+    if result != 0:
+        print("Error unable to load wallet storage", result)
+        parser.print_help()
+        sys.exit(0)
+
+    if args.storage_type == "postgres_storage":
+        try:
+            print("Calling init_storagetype() for postgres:", args.config, args.creds)
+            init_storagetype = stg_lib["init_storagetype"]
+            c_config = c_char_p(args.config.encode('utf-8'))
+            c_credentials = c_char_p(args.creds.encode('utf-8'))
+            result = init_storagetype(c_config, c_credentials)
+            print(" ... returns ", result)
+        except RuntimeError as e:
+            print("Error initializing storage, ignoring ...", e)
+
+    print("Success, loaded wallet storage", args.storage_type)
+
+async def run():
+    logger.info("SDK application started ----------> started")
+
+
+if __name__ == '__main__':
+    run_coroutine(run)
+    time.sleep(1)
